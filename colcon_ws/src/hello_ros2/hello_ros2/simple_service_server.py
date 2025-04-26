603653e709decc272dc2544ae3857f47d867e129
mjjungkor <mjjungkor@naver.com>
2025-04-26 17:41:03 +0900

    ros2 server

--- colcon_ws/src/hello_ros2/hello_ros2/simple_service_server.py
+++ colcon_ws/src/hello_ros2/hello_ros2/simple_service_server.py
@@ -9,6 +9,7 @@ class Service_server(Node):
 
     def setBool_callback(self, request:SetBool.Request, response:SetBool.Response):
         self.bool = request.data
+        self.get_logger().info('This is an info message.')
         response.message='변경 성공'
         response.success=True
         time.sleep(5)
@@ -20,7 +21,7 @@ def main():
     rclpy.init()
     node=Service_server()
     try:
-        rclpy.spin()
+        rclpy.spin(node)
     except KeyboardInterrupt:
         node.destroy_node()
 
